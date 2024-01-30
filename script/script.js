2953fb6cc00821757039002b6c3f139c6b751ffa
seongtaek.hong <seongtaek.hong@looxidlabs.com>
2024-01-30 18:27:23 +0900

    Refactor: stylesheet, css 를 공통 파일로 분리

---
+++ script/script.js
@@ -0,0 +1,10 @@
+const cursorBig = document.querySelector(".cursor");
+
+const positionElement = (e) => {
+  const mouseY = e.clientY;
+  const mouseX = e.clientX;
+
+  cursorBig.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+};
+
+window.addEventListener("mousemove", positionElement);
