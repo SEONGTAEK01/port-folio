4856a1ae4d45473d58be4db63b81a8db6fdf98b5
seongtaek.hong <seongtaek.hong@looxidlabs.com>
2024-01-31 20:53:47 +0900

    Remove: 스크롤링 문제로 커스텀 커서 제거

--- script/script.js
+++ script/script.js
@@ -1,10 +1,10 @@
-const cursorBig = document.querySelector(".cursor");
+// const cursorBig = document.querySelector(".cursor");
 
-const positionElement = (e) => {
-  const mouseY = e.clientY;
-  const mouseX = e.clientX;
+// const positionElement = (e) => {
+//   const mouseY = e.clientY;
+//   const mouseX = e.clientX;
 
-  cursorBig.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
-};
+//   cursorBig.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+// };
 
-window.addEventListener("mousemove", positionElement);
+// window.addEventListener("mousemove", positionElement);
